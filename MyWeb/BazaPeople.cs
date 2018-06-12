99e22a133d1ecf59c11d5cbc69f5b44a592494a3
AbdulazizovVakhid <38040071+AbdulazizovVakhid@users.noreply.github.com>
2018-06-12 21:46:49 +0300

    ?

---
+++ MyWeb/BazaPeople.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MyWeb
+{
+    public class BazaPeople : DbContext
+    {
+        public BazaPeople ()
+        {
+
+        }
+
+        static BazaPeople ()
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<BazaPeople>());
+        }
+
+        public DbSet <People> Persons { get; set; }
+
+    }
+}
---
+++ MyWeb/People.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWeb
+{
+    public class People
+    {
+        public string Name;
+        public string surname;
+        public string Email;
+        public string patronymic; 
+        public string LoanAmount;
+        public string NumberPhone;
+        public override string ToString()
+        {
+            return string.Format(" Имя {0}, Фамилия {1}, Отчество {2}, Мэйл {3} Сумма желаемого займа {4}, " +
+                "Номер телефона {5}" , Name ,surname , patronymic , Email , LoanAmount , NumberPhone);
+        }
+    }
+}
